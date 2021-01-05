4c185a1d18718cc6586a991c7f44aadae14ae167
xialong <625033413@qq.com>
2021-01-05 16:26:10 +0800

    fix实体定义注释

--- MgProductInfSvr/src/main/java/fai/MgProductInfSvr/interfaces/entity/ProductBasicEntity.java
+++ MgProductInfSvr/src/main/java/fai/MgProductInfSvr/interfaces/entity/ProductBasicEntity.java
@@ -24,11 +24,11 @@ public class ProductBasicEntity {
         public static final String AID = "aid"; // int 企业id
         public static final String PD_ID = "pdId"; // int 商品id
         public static final String SOURCE_TID = "sourceTid"; // int 创建商品的项目 id
-        public static final String NAME = "name"; // int 创建商品的项目 id
+        public static final String NAME = "name"; // varchar(255) 商品名称
         public static final String PD_TYPE = "productType"; // int 商品类型
         public static final String IMG_LIST = "imgList"; // varchar(255) 商品图片
         public static final String VIDEO_LIST = "videoList"; // varchar(255) 商品视频
-        public static final String UNIT = "unit"; // varchar 商品视频
+        public static final String UNIT = "unit"; // tinyInt(4) 单位
         public static final String FLAG = "flag"; // int
         public static final String FLAG1 = "flag1"; // int
         public static final String KEEP_PROP1 = "keepProp1"; // varchar(255) 保留字段1

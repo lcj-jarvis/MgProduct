88ee8342c8f15e9ac586e0abce1277ff81d2f07c
whale <faisco123>
2021-01-21 14:22:13 +0800

    去掉无用的包

--- MgProductStoreSvr/src/main/java/fai/MgProductStoreSvr/interfaces/dto/StoreSkuSummaryDto.java
+++ MgProductStoreSvr/src/main/java/fai/MgProductStoreSvr/interfaces/dto/StoreSkuSummaryDto.java
@@ -1,7 +1,6 @@
 package fai.MgProductStoreSvr.interfaces.dto;
 
 import fai.MgProductStoreSvr.domain.entity.StoreSkuSummaryEntity;
-import fai.MgProductStoreSvr.interfaces.entity.SalesSummaryEntity;
 import fai.comm.util.ParamDef;
 import fai.comm.util.Var;
 

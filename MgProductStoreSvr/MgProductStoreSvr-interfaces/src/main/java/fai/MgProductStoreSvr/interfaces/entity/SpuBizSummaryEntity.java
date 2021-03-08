39fd5c47d05f28a972906c9e4c3ed502d8f5ab6e
whale <faisco123>
2021-03-08 10:50:29 +0800

    增加访客态字段和管理态字段获取

--- MgProductStoreSvr/MgProductStoreSvr-interfaces/src/main/java/fai/MgProductStoreSvr/interfaces/entity/SpuBizSummaryEntity.java
+++ MgProductStoreSvr/MgProductStoreSvr-interfaces/src/main/java/fai/MgProductStoreSvr/interfaces/entity/SpuBizSummaryEntity.java
@@ -1,5 +1,7 @@
 package fai.MgProductStoreSvr.interfaces.entity;
 
+import java.util.Arrays;
+
 /**
  * spu 业务库存销售汇总表
  */
@@ -28,4 +30,40 @@ public class SpuBizSummaryEntity {
         public static final String KEEP_INT_PROP1 = "keepIntProp1";                 // int 整型 保留字段1 TODO
     }
 
+    private static final String[] VISITOR_KEYS = new String[]{
+            Info.SALES
+            , Info.COUNT
+            , Info.REMAIN_COUNT
+            , Info.HOLDING_COUNT
+    };
+    /**
+     * 获得访客字段
+     */
+    public static String[] getVisitorKeys(){
+        return VISITOR_KEYS;
+    }
+
+    private static final String[] MANAGE_KEYS = new String[]{
+            Info.MARKET_PRICE
+            , Info.MIN_PRICE
+            , Info.MAX_PRICE
+    };
+    /**
+     * 获取管理字段
+     */
+    public static String[] getManageKeys(){
+        return MANAGE_KEYS;
+    }
+
+    private static String[] MANAGE_VISITOR_KEYS;
+    static {
+        MANAGE_VISITOR_KEYS = Arrays.copyOf(VISITOR_KEYS, VISITOR_KEYS.length + MANAGE_KEYS.length);
+        System.arraycopy(MANAGE_KEYS, 0, MANAGE_VISITOR_KEYS, VISITOR_KEYS.length, MANAGE_KEYS.length);
+    }
+    /**
+     * 获取管理和访客字段
+     */
+    public static String[] getManageVisitorKeys(){
+        return MANAGE_VISITOR_KEYS;
+    }
 }

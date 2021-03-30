14741d7976e97b9c7944d84192367055ca296081
whale <faisco123>
2021-03-30 10:21:14 +0800

    fix

--- MgProductInfUtil/src/main/java/fai/MgProductInfUtil/MgProductInternalCli.java
+++ MgProductInfUtil/src/main/java/fai/MgProductInfUtil/MgProductInternalCli.java
@@ -7,8 +7,8 @@ import fai.middleground.infutil.MgInternalCli;
  */
 public class MgProductInternalCli extends MgInternalCli {
     public MgProductInternalCli(int flow, String name) {
-        //-Dfai.MgProductInfUti.MgProductInternalCli.allow=true
-        super(flow, name, "fai.MgProductInfUti.MgProductInternalCli.allow");
+        //-Dfai.MgProductInfUtil.MgProductInternalCli.allow=true
+        super(flow, name, "fai.MgProductInfUtil.MgProductInternalCli.allow");
     }
 
 }

76df6db644919050cfcb7cf9451487b7ec520a19
whale <faisco123>
2021-03-30 10:20:11 +0800

    修正报名，和注释

--- MgProductInfUtil/src/main/java/fai/MgProductInfUtil/MgProductInternalCli.java
+++ MgProductInfUtil/src/main/java/fai/MgProductInfUtil/MgProductInternalCli.java
@@ -1,7 +1,10 @@
-package fai.MgProductInfUti;
+package fai.MgProductInfUtil;
 
 import fai.middleground.infutil.MgInternalCli;
 
+/**
+ * 商品中台内部接口cli
+ */
 public class MgProductInternalCli extends MgInternalCli {
     public MgProductInternalCli(int flow, String name) {
         //-Dfai.MgProductInfUti.MgProductInternalCli.allow=true

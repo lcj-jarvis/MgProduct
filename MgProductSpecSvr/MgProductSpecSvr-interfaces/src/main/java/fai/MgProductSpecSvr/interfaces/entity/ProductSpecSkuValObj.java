4d37abc2568b1cda4835e57d4036edb7ecdd99dd
xialong <625033413@qq.com>
2021-06-01 16:19:10 +0800

    skuCode最大长度调整为32

--- MgProductSpecSvr/MgProductSpecSvr-interfaces/src/main/java/fai/MgProductSpecSvr/interfaces/entity/ProductSpecSkuValObj.java
+++ MgProductSpecSvr/MgProductSpecSvr-interfaces/src/main/java/fai/MgProductSpecSvr/interfaces/entity/ProductSpecSkuValObj.java
@@ -12,7 +12,7 @@ public class ProductSpecSkuValObj {
         public static final int SINGLE_PRODUCT_MAX_SIZE = 10000;
         public static final class SkuNum {
             public static final int MIN_LEN = 1; // 最小长度
-            public static final int MAX_LEN = 20; // 最大长度
+            public static final int MAX_LEN = 32; // 最大长度
         }
         public static final class InPdScValIdSkuList{
             public static final int MAX_SIZE = 15; // 最多15个元素

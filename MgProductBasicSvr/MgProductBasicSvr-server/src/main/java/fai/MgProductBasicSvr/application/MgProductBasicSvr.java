aa02467f3237f3c97c50cf8c123b8771938ec9d4
GuoYuYuan <244164028@qq.com>
2021-07-22 15:23:33 +0800

    test

--- MgProductBasicSvr/MgProductBasicSvr-server/src/main/java/fai/MgProductBasicSvr/application/MgProductBasicSvr.java
+++ MgProductBasicSvr/MgProductBasicSvr-server/src/main/java/fai/MgProductBasicSvr/application/MgProductBasicSvr.java
@@ -60,6 +60,10 @@ public class MgProductBasicSvr {
 
         server.setHandler(new MgProductBasicHandler(server));
         server.start();
+		
+		//test
+		
+		//test
     }
 
     private static DaoPool getDaoPool(String svrName, int type, int maxSize, String instance) {

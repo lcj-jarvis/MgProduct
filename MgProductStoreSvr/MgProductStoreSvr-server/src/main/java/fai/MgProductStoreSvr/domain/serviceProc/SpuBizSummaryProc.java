472cbbfbcd1ed2c4d7c4f3269c86e24fb49a564b
GuoYuYuan <244164028@qq.com>
2022-01-14 15:08:30 +0800

    fix insert empty data

--- MgProductStoreSvr/MgProductStoreSvr-server/src/main/java/fai/MgProductStoreSvr/domain/serviceProc/SpuBizSummaryProc.java
+++ MgProductStoreSvr/MgProductStoreSvr-server/src/main/java/fai/MgProductStoreSvr/domain/serviceProc/SpuBizSummaryProc.java
@@ -181,10 +181,12 @@ public class SpuBizSummaryProc {
                 return rt;
             }
         }
-        rt = m_daoCtrl.batchInsert(list, null, true);
-        if(rt != Errno.OK) {
-            Log.logErr(rt, "copyBizBind error;flow=%d;aid=%d;fromUid=%s;toUid=%s;", m_flow, aid, fromUnionPriId, toUnionPriId);
-            return rt;
+        if (!Utils.isEmptyList(list)) {
+            rt = m_daoCtrl.batchInsert(list, null, true);
+            if(rt != Errno.OK) {
+                Log.logErr(rt, "copyBizBind error;flow=%d;aid=%d;fromUid=%s;toUid=%s;", m_flow, aid, fromUnionPriId, toUnionPriId);
+                return rt;
+            }
         }
 
         Log.logStd("copyBizBind ok;flow=%d;aid=%d;fromUid=%s;toUid=%s;", m_flow, aid, fromUnionPriId, toUnionPriId);

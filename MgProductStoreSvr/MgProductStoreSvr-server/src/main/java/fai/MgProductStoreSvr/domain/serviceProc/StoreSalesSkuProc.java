f7eafd1344df0587bb9cf1f9cefd512edff027c3
GuoYuYuan <244164028@qq.com>
2022-03-21 20:45:51 +0800

    fix-库存销售删除异常

--- MgProductStoreSvr/MgProductStoreSvr-server/src/main/java/fai/MgProductStoreSvr/domain/serviceProc/StoreSalesSkuProc.java
+++ MgProductStoreSvr/MgProductStoreSvr-server/src/main/java/fai/MgProductStoreSvr/domain/serviceProc/StoreSalesSkuProc.java
@@ -280,7 +280,7 @@ public class StoreSalesSkuProc {
         matcher.and(StoreSalesSkuEntity.Info.SKU_ID, ParamMatcher.IN, delSkuIdList);
 
         if (isSaga) {
-            rt = addDelOp4Saga(aid, matcher);
+            rt = addDelOp4Saga(aid, matcher.clone());
             if (rt != Errno.OK) {
                 return rt;
             }
